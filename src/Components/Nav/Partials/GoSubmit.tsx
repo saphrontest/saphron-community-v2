53f67d3e3270a8f6f51c8442bb79f45b7951f33a
kadricaabuk <kadricabuk@gmail.com>
2023-11-19 12:57:22 +0300

    ask question button

--- src/Components/Nav/Partials/GoSubmit.tsx
+++ src/Components/Nav/Partials/GoSubmit.tsx
@@ -1,4 +1,4 @@
-import { Flex, Icon } from '@chakra-ui/react'
+import { Flex, Icon, Text } from '@chakra-ui/react'
 import { GrAdd } from "react-icons/gr";
 import { useNavigate } from 'react-router-dom';
 
@@ -8,15 +8,23 @@ const GoSubmit = () => {
         <Flex alignItems="center" flexGrow={1}>
             <Flex
                 display={{ base: "none", md: "flex" }}
+                justify="center"
+                align="center"
+                gap={2}
                 mr={3}
                 ml={1.5}
                 padding={1}
                 cursor="pointer"
                 borderRadius={4}
-                _hover={{ bg: "gray.200" }}
+                outline={"1px solid"}
+                outlineColor={"gray.100"}
+                _hover={{ outline: "1px solid", outlineColor: "gray.200" }}
                 onClick={() => navigate("/submit")}
             >
                 <Icon as={GrAdd} fontSize={20} color="red.100"/>
+                <Text fontSize={16} fontWeight={500} color="black.500">
+                    Ask Question
+                </Text>
             </Flex>
         </Flex>
     )

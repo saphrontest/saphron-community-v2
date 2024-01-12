0850602ca40f4fa5b0247d070b92eec76292ba14
kadricaabuk <kadricabuk@gmail.com>
2024-01-12 10:08:52 +0300

    ai integration

--- src/Components/Search/AskAI.tsx
+++ src/Components/Search/AskAI.tsx
@@ -1,17 +1,42 @@
-import { Button, Flex, Stack, Textarea } from '@chakra-ui/react'
-import React, { useState } from 'react'
+import { Box, Button, Flex, Heading, Stack, Text, Textarea } from '@chakra-ui/react'
+import React, { useEffect, useState } from 'react'
 import AIDialog from './AIDialog'
 import SuggestedQuestions from './SuggestedQuestions'
+import { openai } from '../../openAIClient'
+import { ChatCompletionCreateParamsNonStreaming } from 'openai/resources'
 
 const AskAI = () => {
   const [text, setText] = useState<string>("")
+  const [params, setParams] = useState<ChatCompletionCreateParamsNonStreaming>({
+    messages: [{ role: "system", content: "You are a helpful assistant."}],
+    model: "gpt-3.5-turbo",
+  })
+  const [sendMessage, setSendMessage] = useState<boolean>(false)
+  
 
-  const handleAsk = () => {
-    console.log(text)
+  const send = async () => {
+    console.log(params)
+    const response = await openai.chat.completions.create(params);
+    console.log(response)
   }
+
+  useEffect(() => {
+    if(sendMessage) {
+      send()
+      return () => setSendMessage(false)
+    }
+  }, [sendMessage])
 
   return (
     <Flex p="1" width="100%" direction="column">
+      <Box  m={2} mt={0}>
+        <Heading size="md" textAlign="left">
+          Ask to Saphron AI
+        </Heading>
+        <Text textAlign="left">
+          You can ask the Saphron AI, which is developed using ChatGPT for our community members.
+        </Text>
+      </Box>
       <Flex direction="column" width="100%">
         <Textarea  width="100%" onChange={e => setText(e.target.value)}/>
         <Stack w={"100%"} display={"flex"} align="flex-end" pt={"1rem"}>
@@ -19,7 +44,13 @@ const AskAI = () => {
             w="fit-content" 
             height="34px"
             padding="0px 30px"
-            onClick={handleAsk}
+            onClick={() => {
+              setParams(prev => ({
+                messages: [...prev.messages, {role: 'user', content: text}],
+                model: "gpt-3.5-turbo"
+              }))
+              setSendMessage(true)
+            }}
           >
             Ask to Saphron AI
           </Button>

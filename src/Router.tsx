112d0333b2b224f29c67760b31407f943ac18198
kadricaabuk <kadricabuk@gmail.com>
2024-01-26 18:20:54 +0300

    update router

--- src/Router.tsx
+++ src/Router.tsx
@@ -2,43 +2,15 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import {CommunityDetail, Home, PostDetail, SavedPosts, Submit, Profile, SearchPage, WorkshopsPage} from './Pages'
 
 const RoutesArray = [
-  {
-    path: "/",
-    component: Home, 
-  },
-  {
-    path: "submit",
-    component: Submit, 
-  },
-  {
-    path: "submit/:communityId",
-    component: Submit, 
-  },
-  {
-    path: "community-detail/:id",
-    component: CommunityDetail, 
-  },
-  {
-    path: "post/:slug",
-    component: PostDetail, 
-  },
-  {
-    path: "saved-posts",
-    component: SavedPosts, 
-  },
-  {
-    path: "profile",
-    component: Profile
-  },
-  {
-    path: "search",
-    component: SearchPage
-  },
-  {
-    path: "workshops",
-    component: WorkshopsPage
-  },
-
+  { path: "/", component: Home },
+  { path: "submit", component: Submit },
+  { path: "submit/:communityId", component: Submit },
+  { path: "community-detail/:id", component: CommunityDetail },
+  { path: "post/:slug", component: PostDetail },
+  { path: "saved-posts", component: SavedPosts },
+  { path: "profile", component: Profile },
+  { path: "search", component: SearchPage },
+  { path: "workshops", component: WorkshopsPage },
 ]
 
 const Router = () => {

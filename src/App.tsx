9446c26d1d41bf06a3e64ff74d49b5e47774c706
kadricaabuk <kadricabuk@gmail.com>
2024-02-27 17:14:09 +0300

    update

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,8 @@ import Router from './Router';
 import './App.css';
 import { Capacitor } from '@capacitor/core';
 import { Box } from '@chakra-ui/react';
-import { useEffect } from 'react';
-import { SplashScreen } from '@capacitor/splash-screen';
 
 function App() {
-
-  useEffect(() => {
-    // SplashScreen.
-  }, [])
-
   return (
     <>
       {Capacitor.getPlatform() === 'ios' && <meta name="viewport" content="width=device-width, initial-scale=1.0, viewport-fit=cover, user-scalable=no" />  }

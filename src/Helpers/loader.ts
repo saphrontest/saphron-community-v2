043effd2b05f101b512560b75de037a062dc0e6b
kadricaabuk <kadricabuk@gmail.com>
2024-03-13 12:23:33 +0300

    last commit

--- src/Helpers/loader.ts
+++ src/Helpers/loader.ts
@@ -1,6 +1,7 @@
 import { query, collection, where, limit, getDocs, doc, getDoc } from "firebase/firestore";
 import { IPost, Community } from "../Interface";
 import { firestore } from "../firebaseClient";
+import { LoaderFunctionArgs } from "react-router-dom";
 
 export const getPostDetail = async (slugId: string) => {
   try {
@@ -36,44 +37,5 @@ export const getCommunityDetails = async (id: string) => {
   }
 };
 
-
-
-
-// import { query, collection, where, limit, getDocs, doc, getDoc } from "firebase/firestore";
-// import { IPost, Community } from "../Interface";
-// import { firestore } from "../firebaseClient";
-// import { LoaderFunctionArgs } from "react-router-dom";
-
-// export const postDetailLoader = async ({ params: { slugId } }: LoaderFunctionArgs) => {
-//   try {
-//     const q = query(
-//       collection(firestore, "posts"),
-//       where("slugId", "==", slugId),
-//       limit(1)
-//     );
-//     const querySnapshot = await getDocs(q);
-
-//     if (querySnapshot.size === 0) {
-//       // Return null or handle the case where no matching post is found
-//       return null;
-//     }
-
-//     const doc = querySnapshot.docs[0];
-//     return { id: doc.id, ...doc.data() } as IPost;
-//   } catch (error) {
-//     console.error("Error getting post details:", error);
-//     throw error; // Rethrow the error to handle it at a higher level if needed
-//   }
-// };
-
-// export const communityDetailLoader = async ({ params: { communityId } }: LoaderFunctionArgs) => {
-//   try {
-//     const communityDetailRef = doc(firestore, "communities", communityId as string);
-//     const community = await getDoc(communityDetailRef);
-//     const communityObject = { id: community.id, ...community.data() };
-//     return communityObject as Community;
-//   } catch (error) {
-//     console.error("Error getting community details:", error);
-//     throw error; // Rethrow the error to handle it at a higher level if needed
-//   }
-// };
+export const postDetailLoader = async ({ params: { slugId } }: LoaderFunctionArgs) => await getPostDetail(slugId as string)
+export const communityDetailsLoader = async ({ params: { communityId } }: LoaderFunctionArgs) => await getCommunityDetails(communityId as string)
--- src/Router.tsx
+++ src/Router.tsx
@@ -23,54 +23,59 @@ import {
 import { getCommunityDetails, getPostDetail } from './Helpers'
 
 const BASE = "community"
-const WORKSHOPS_BASE = "workshops"
-const SUPPORT_GROUPS_BASE = "workshops"
-const ADMIN_BASE = "manager-dashboard"
+const WORKSHOPS_BASE = "/workshops"
+const SUPPORT_GROUPS_BASE = "/workshops"
+const ADMIN_BASE = "/manager-dashboard"
 
 const router = createBrowserRouter(
   createRoutesFromElements(
-    <Route path='/'>
+    <Route>
 
-      <Route element={<Home />} index />
+      <Route path='/' element={<Home />} index />
       <Route path='about' element={<AboutPage />} />
       <Route path='my-workshops' element={<MyWorkshopsPage />} />
       <Route path='my-support-groups' element={<MySupportGroups />} />
       <Route path='privacy-policy' element={<Policies />} />
       <Route path='terms-conditions' element={<Policies />} />
 
-      <Route path={BASE} element={<Home />}>
+      <Route path={BASE}>
 
+        <Route path='/' element={<Home />} index/>
         <Route path='saved-posts' element={<SavedPosts />} />
         <Route path='profile' element={<Profile />} />
         <Route path='search' element={<SearchPage />} />
 
         <Route path='submit'>
-          <Route element={<Submit />} index />
+          <Route path='/' element={<Submit />} index />
           <Route path=':communityId' element={<Submit />} />
         </Route>
 
-        <Route path='community-detail'>
-          <Route path=':communityId' element={<CommunityDetail />} loader={async ({ params: { communityId } }) => await getCommunityDetails(communityId as string)} />
+        <Route path='community-detail/'>
+          <Route index path=':communityId' element={<CommunityDetail />} loader={async ({ params: { communityId } }) => await getCommunityDetails(communityId as string)} />
         </Route>
 
-        <Route path='post'>
+        <Route path='post/'>
           <Route path=':slugId' element={<PostDetail />} loader={async ({ params: { slugId } }) => await getPostDetail(slugId as string)} />
-          <Route path=':slugId/:slug' element={<PostDetail />} loader={async ({ params: { slugId } }) => await getPostDetail(slugId as string)} />
+          <Route index path=':slugId/:slug' element={<PostDetail />} loader={async ({ params: { slugId } }) => await getPostDetail(slugId as string)} />
         </Route>
       </Route>
 
-      <Route path={WORKSHOPS_BASE} element={<WorkshopsPage />}>
+      <Route path={WORKSHOPS_BASE}>
+        <Route index path='/' element={<WorkshopsPage />} />
         <Route path=':slug' element={<WorkshopsPage />} />
       </Route>
-      
-      <Route path={ADMIN_BASE} element={<AdminPage />}>
+
+      <Route path={ADMIN_BASE}>
+
+        <Route path='/' element={<AdminPage />} index/>
         <Route path='posts' element={<AdminPostsPage />} />
         <Route path='users' element={<AdminUsersPage />} />
         <Route path='workshops' element={<AdminWorkshopsPage />} />
         <Route path='support-groups' element={<AdminSupportGroupsPage />} />
       </Route>
-      
-      <Route path={SUPPORT_GROUPS_BASE} element={<SupportGroups />}>
+
+      <Route path={SUPPORT_GROUPS_BASE}>
+        <Route path='/' element={<SupportGroups />} />
         <Route path=':slug' element={<SupportGroupDetailPage />} />
       </Route>
 
@@ -78,6 +83,90 @@ const router = createBrowserRouter(
     </Route>
   )
 )
+
+// const RoutesArray = createBrowserRouter([
+//   { path: "/", element: <Home />, index: true},
+//   { path: "/profile", element: <Profile /> },
+//   { path: "/search", element: <SearchPage /> },
+//   { path: "/about", element: <AboutPage /> },
+//   { path: "/submit", element: <Submit /> },
+//   { path: "/my-support-groups", element: <MySupportGroups /> },
+//   { path: "/privacy-policy", element: <Policies /> },
+//   { path: "/terms-conditions", element: <Policies /> },
+//   { path: "/my-workshops", element: <MyWorkshopsPage /> },
+//   {
+//     path: BASE,
+//     element: <Home />,
+//     children: [
+//       {
+//         path: `/`,
+//         element: <Home />,
+//         index: true
+//       },
+//       {
+//         path: `/submit`,
+//         element: <Submit />,
+//       },
+//       {
+//         path: `/submit/:communityId`,
+//         element: <Submit />
+//       },
+//       {
+//         path: `/community-detail/:communityId`,
+//         element: <CommunityDetail />,
+//         loader: async ({ params: { communityId } }) => await getCommunityDetails(communityId as string)
+//       },
+//       {
+//         path: `/post/:slugId/:slug`,
+//         element: <PostDetail />,
+//         loader: async ({ params: { slugId } }) => await getPostDetail(slugId as string)
+//       },
+//       {
+//         path: `/saved-posts`,
+//         element: <SavedPosts />
+//       }
+//     ]
+//   },
+//   {
+//     path: WORKSHOPS_BASE,
+//     element: <WorkshopsPage />,
+//     children: [
+//       {
+//         path: `/`,
+//         element: <WorkshopsPage />,
+//         index: true
+//       },
+//       { path: "/:slug", element: <WorkshopsPage /> },
+//     ]
+//   },
+//   {
+//     path: SUPPORT_GROUPS_BASE,
+//     element: <SupportGroups />,
+//     children: [
+//       {
+//         path: `/`,
+//         element: <SupportGroupDetailPage />,
+//         index: true
+//       },
+//       { path: "/support-groups/:slug", element: <SupportGroupDetailPage /> },
+//     ]
+//   },
+//   {
+//     path: ADMIN_BASE,
+//     element: <AdminPage />,
+//     children: [
+//       {
+//         path: `/`,
+//         element: <AdminPage />,
+//       },
+//       { path: `/posts`, element: <AdminPostsPage /> },
+//       { path: `/users`, element: <AdminUsersPage /> },
+//       { path: `/workshops`, element: <AdminWorkshopsPage /> },
+//       { path: `/support-groups`, element: <AdminSupportGroupsPage /> },
+
+//     ]
+//   },
+// ])
 
 const Router = () => (
   <RouterProvider router={router} />

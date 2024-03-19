25132a74fa7de1cf5bef042468b2494aece6d823
kadricaabuk <kadricabuk@gmail.com>
2024-03-19 16:41:01 +0300

    update badge component

--- src/Components/Membership/MembershipBadge.tsx
+++ src/Components/Membership/MembershipBadge.tsx
@@ -9,13 +9,37 @@ const MembershipBadge: FC<{ membershipType: string; showName?: boolean; }> = ({
 
   const dispatch = useDispatch()
 
-  const membership = useMemo(() => {
-    return {
-      bg: membershipType === 'Gold' ? 'yellow.300' : membershipType === 'Silver' ? 'gray.500' : 'gray.100',
-      text: membershipType === 'Gold' ? 'G' : membershipType === 'Silver' ? 'S' : 'P',
-      textColor: membershipType === 'Platinium' ? 'black' : 'white'
+  interface MembershipJson {
+    [membershipType: string]: {
+      bg: string;
+      text: string;
+      textColor: string;
     }
+  }
+
+  const MEMBERSHIP_JSON: MembershipJson = {
+    'Gold': {
+      bg: 'yellow.300',
+      text: 'G',
+      textColor: 'white'
+    },
+    'Silver': {
+      bg: 'gray.500',
+      text: 'S',
+      textColor: 'white'
+    },
+    'Platinium': {
+      bg: 'gray.100',
+      text: 'P',
+      textColor: 'black'
+    }
+  }
+
+  const membership = useMemo(() => {
+    return MEMBERSHIP_JSON[membershipType]
   }, [membershipType])
+
+  
 
   return membershipType === '' ? (
     <Button onClick={() => dispatch(setModal({ isOpen: true, view: 'paymentModal', data: { type: 'membership' } }))}>

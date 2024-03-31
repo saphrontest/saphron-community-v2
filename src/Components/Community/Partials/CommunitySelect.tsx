cb0dd087a87b1f831ec91d4add0ec8da9d81032e
kadricaabuk <kadricabuk@gmail.com>
2024-03-31 14:44:44 +0300

    community select parsed

--- src/Components/Community/Partials/CommunitySelect.tsx
+++ src/Components/Community/Partials/CommunitySelect.tsx
@@ -1,15 +1,16 @@
 import { ChevronDownIcon } from '@chakra-ui/icons';
 import { Box, Flex, Icon, Menu, MenuButton, MenuDivider, MenuItem, MenuList, Text, useOutsideClick } from '@chakra-ui/react';
-import { FC, useEffect, useRef, useState } from 'react'
+import { FC, Fragment, useEffect, useRef, useState } from 'react'
 import { GrAdd } from 'react-icons/gr';
 import { useDispatch, useSelector } from 'react-redux';
 import { setModal } from '../../../redux/slices/modalSlice';
 import { useNavigate } from 'react-router-dom';
 import { setCommunities, setJoinedCommunities, setSelectedCommunity } from '../../../redux/slices/communitySlice';
 import { RootState } from '../../../redux/store';
-import { Community, JoinedCommunity } from '../../../Interface';
+import { Community, IUser, JoinedCommunity } from '../../../Interface';
 import NoEntry from '../../NoEntry';
 import { useCommunity } from '../../../Hooks';
+import CommunitySelectSegment from './CommunitySelectSegment';
 
 interface CommunityProps {
     isOpen: boolean;
@@ -24,27 +25,31 @@ const CommunitySelect: FC<CommunityProps> = ({
     isOpen,
     setOpen,
     selectedCommunityId,
-    showTitleOnMobile=true
+    showTitleOnMobile = true
 }) => {
-    const {getCommunities, getJoinedCommunities} = useCommunity()
+
+    const communityMenuRef = useRef(null)
+
     const navigate = useNavigate()
     const dispatch = useDispatch()
-    const user = useSelector((state: RootState) => state.user)
-    const communityMenuRef = useRef(null)
+    const { getCommunities, getJoinedCommunities } = useCommunity()
+
     const [formattedCommunities, setFormattedCommunities] = useState<any[]>([])
-    const {communities, selectedCommunity, joinedCommunities} = useSelector((state: RootState) => state.community)
-    
+
+    const user: IUser = useSelector((state: RootState) => state.user)
+    const { communities, selectedCommunity, joinedCommunities } = useSelector((state: RootState) => state.community)
+
     useOutsideClick({
         ref: communityMenuRef,
         handler: () => isOpen && setOpen(!isOpen)
-      });
+    });
 
 
     const getCommunityList = async () => {
         const res = await getCommunities()
         dispatch(setCommunities(res as Community[]))
     }
-    
+
     const getJoinedCommunitiesList = async (userId: string) => {
         const joined = await getJoinedCommunities(userId)
         joined && dispatch(setJoinedCommunities(joined))
@@ -52,37 +57,41 @@ const CommunitySelect: FC<CommunityProps> = ({
 
     useEffect(() => {
         user?.id && getJoinedCommunitiesList(user?.id)
-    // eslint-disable-next-line react-hooks/exhaustive-deps
+        // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [user])
 
-    useEffect(() => {    
+    useEffect(() => {
         getCommunityList()
-        if(selectedCommunityId){
-            const comm = communities.find((community: Community) => community.id === selectedCommunityId)
+        if (selectedCommunityId) {
+            const comm = communities.find(
+                (community: Community) => community.id === selectedCommunityId
+            )
             comm && dispatch(setSelectedCommunity(comm))
         }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
+        // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [])
 
+    const handleClick = (comm: any) => {
+        dispatch(setSelectedCommunity(comm))
+        !!isNav && navigate(`/community/community-detail/${comm.id}`)
+        setOpen(false)
+    }
 
     useEffect(() => {
-        if(joinedCommunities.length && communities.length) {
+        if (joinedCommunities.length && communities.length) {
             setFormattedCommunities([])
 
             joinedCommunities.forEach((joined: JoinedCommunity) => {
                 communities.forEach((community: Community) => {
-                    if(joined.communityId === community.id) {
-                        setFormattedCommunities(prev => ([{isModerator: joined?.isModerator, ...community}, ...prev])) 
+                    if (joined.communityId === community.id) {
+                        setFormattedCommunities(prev => ([{ isModerator: joined?.isModerator, ...community }, ...prev]))
                     }
-                    
+
                 });
             })
 
         }
     }, [communities, joinedCommunities])
-
-    
-
 
     return (
         <Menu isOpen={isOpen}>
@@ -106,86 +115,46 @@ const CommunitySelect: FC<CommunityProps> = ({
                     width={{ base: "auto", lg: "200px" }}
                 >
                     <Text
-                    fontWeight={600}
-                    display={{ base: !showTitleOnMobile ? "none" : "flex", lg: "flex" }}
-                    flexDirection="column"
-                    fontSize="10pt"
+                        fontWeight={600}
+                        display={{ base: !showTitleOnMobile ? "none" : "flex", lg: "flex" }}
+                        flexDirection="column"
+                        fontSize="10pt"
                     >{selectedCommunity?.name ?? "Select Community"}</Text>
                     <ChevronDownIcon color="gray.500" />
                 </Flex>
             </MenuButton>
             <MenuList ref={communityMenuRef} zIndex={9999}>
                 {
-                    !!formattedCommunities.length === false ? <NoEntry type="community"/> : (
+                    !!formattedCommunities.length === false ? <NoEntry type="community" /> : (
                         <>
-                        
-                <Box mt={3} mb={4}>
-                    {!!formattedCommunities.filter(com => com.isModerator).length ? <Text
-                        pl={3}
-                        mb={1}
-                        fontSize="7pt"
-                        fontWeight={500}
-                        color="gray.500"
-                    >
-                        MODERATING
-                    </Text> : null}
-                    {formattedCommunities.filter(com => com.isModerator).map(comm => {
-                        return (
-                            <MenuItem
-                                key={comm.id}
-                                width="100%"
-                                fontSize="10pt"
-                                fontWeight={600}
-                                _hover={{ bg: "gray.100" }}
-                                onClick={() => {
-                                    dispatch(setSelectedCommunity(comm))
-                                    !!isNav && navigate(`/community/community-detail/${comm.id}`)
-                                    setOpen(false)
-                                }}
-                            >
-                                <Flex alignItems="center">{comm.name}</Flex>
-                            </MenuItem>
-                        )
-                    })}
-                    
-                </Box>
-                <Box mt={3} mb={4}>
-                {!!formattedCommunities.filter(com => !com.isModerator).length && <Text pl={3} mb={1} fontSize="7pt" fontWeight={500} color="gray.500">
-                MY COMMUNITIES
-                </Text>}
-                {formattedCommunities.filter(com => !com.isModerator).map(comm => {
-                    return (
-                        <MenuItem
-                            key={comm.id}
-                            width="100%"
-                            fontSize="10pt"
-                            fontWeight={600}
-                            _hover={{ bg: "gray.100" }}
-                            onClick={() => {
-                                dispatch(setSelectedCommunity(comm))
-                                !!isNav && navigate(`/community/community-detail/${comm.id}`)
-                                setOpen(false)
-                            }}
-                        >
-                            <Flex alignItems="center">{comm.name}</Flex>
-                        </MenuItem>
-                    )
-                })}
-                <MenuDivider />
-                <MenuItem
-                width="100%"
-                fontSize="10pt"
-                _hover={{ bg: "gray.100" }}
-                onClick={() => dispatch(setModal({isOpen: true, view: "addCommunity"}))}
-                >
-                <Flex alignItems="center">
-                    <Icon fontSize={20} mr={2} as={GrAdd} />
-                    Create Community
-                </Flex>
-                </MenuItem>
-            </Box>
+                            <Box mt={3} mb={4}>
+                                <CommunitySelectSegment
+                                    communities={formattedCommunities.filter(com => com.isModerator)}
+                                    onItemClick={(comm: any) => handleClick(comm)}
+                                    title='MODERATING'
+                                />
+                            </Box>
+                            <Box mt={3} mb={4}>
+                                <CommunitySelectSegment
+                                    communities={formattedCommunities.filter(com => !com.isModerator)}
+                                    onItemClick={(comm: any) => handleClick(comm)}
+                                    title='MY COMMUNITIES'
+                                />
+                                <MenuDivider />
+                                <MenuItem
+                                    width="100%"
+                                    fontSize="10pt"
+                                    _hover={{ bg: "gray.100" }}
+                                    onClick={() => dispatch(setModal({ isOpen: true, view: "addCommunity" }))}
+                                >
+                                    <Flex alignItems="center">
+                                        <Icon fontSize={20} mr={2} as={GrAdd} />
+                                        Create Community
+                                    </Flex>
+                                </MenuItem>
+                            </Box>
 
-            </>
+                        </>
                     )}
             </MenuList>
         </Menu>
